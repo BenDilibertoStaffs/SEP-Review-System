9d894334a18b2e611d163bbb1b4ffd1df71f6b0b
Ben Diliberto <d025226l@student.staffs.ac.uk>
2021-11-07 15:21:57 +0000

    Added API Spring config

---
+++ api/src/main/java/com/sep/restarauntreview/RestarauntReviewSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sep.restarauntreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestarauntReviewSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestarauntReviewSystemApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/sep/restarauntreview/RestarauntReviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep.restarauntreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestarauntReviewSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
