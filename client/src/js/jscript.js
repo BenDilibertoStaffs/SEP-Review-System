e7bdc09d994393366c3c46d8e036ab37c23af68c
Charlotte Norford <cnorfwork@yahoo.com>
2022-01-11 20:20:56 +0000

    feat: login.html: remove action attribute from form and header.html data-bs-target="#sign_up_modal", sign up functionality

--- client/src/js/jscript.js
+++ client/src/js/jscript.js
@@ -1,36 +1,90 @@
-$(function() ({
-   //allow for an application to send
-    $("#submit-sign-up").click(  function(){
+$(function () {
+  //allow for an application to send
+
+  $("form").click(function (e) {
+    e.preventDefault();
+    return false;
+  });
+
+close("#sign-up_modal", "Success: You have created an account" );
 
 
-      return (
-      let passwordFirstEntry = $("#password-1-sign-up").val();
-      let passwordFirstEntry = $("#username-sign-up").val();
+function close(modal, successMsg){
 
-         $.ajax({
-             url: "http://localhost:1234/api/user",
-             type: "post",
-         dataType: "json",
-             data: "username" + ":" + joe123 + "," + "password" + ":" + passwordFirstEntry
-         }))
+  $(modal + " button:first").click(function () {
 
-       }
+         reset(modal, "Success: You have created an account");
 
-    $("#submit-login-in").click(  function(){
+      }
+
+   )
+}
 
 
-               return (
-               let passwordFirstEntry = $("#password-1-sign-up").val();
-               let passwordFirstEntry = $("#username-sign-up").val();
+function reset(modal, successMsg){
+$(modal + " .alert-success:first" ).addClass("d-none");
 
-                  $.ajax({
-                      url: "http://localhost:1234/api/user",
-                      type: "post",
-                  dataType: "json",
-                      data: "username" + ":" + joe123 + "," + "password" + ":" + passwordFirstEntry
-                  }))
-
-                }
+  $(modal + " .alert-danger:first").addClass("d-none");
 
 
-}) }
+  $(modal + " .alert-success:first" ).empty();
+
+ $(modal + " .alert-success:first" ).text(successMsg);
+}
+
+ $("#submit-sign-up").click(function (e) {
+
+    let passwordFirstEntry = $("#password-1-sign-up").val() + " ";
+
+    let passwordSecondEntry = $("#password-2-sign-up").val() + " ";
+
+
+    let isSuccessful = false;
+
+    let usernameEntry = $("#username-sign-up").val() + " ";
+
+    if(passwordFirstEntry == passwordSecondEntry){
+
+    let sign_up = {username: usernameEntry, password: passwordFirstEntry};
+
+    reset("#sign-up_modal");
+
+    ajaxTemplate("#sign-up_modal", "user/", "POST", sign_up, "Success: You have created an account");
+    }
+    else{
+
+    $( "#sign-up_modal .alert-danger" ).removeClass("d-none");
+    $( "#sign-up_modal .alert-danger" ).text("Error: the passwords don't match");
+   }
+
+   reset("#sign-up_modal", "Success: You have created an account" );
+ });
+
+
+  function ajaxTemplate(modal,mapping, ajaxMethod, userObject, successMsg) {
+    let json = JSON.stringify(userObject);
+
+
+     $.ajax({
+      url: "http://localhost:1234/api/" + mapping,
+      contentType: "application/json",
+      method: ajaxMethod,
+      data: json,
+      dataType: "text",
+
+      success: function (data) {
+      $(modal + " .alert-success" ).removeClass("d-none") ;
+
+      $(modal + " .alert-success" ).text(successMsg + ". " + data);
+      },
+
+      error: function (data) {
+
+      $( modal + " .alert-danger" ).removeClass("d-none") ;
+
+
+
+      }
+    });
+  }
+});
